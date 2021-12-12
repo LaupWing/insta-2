6a584883aa26b819054045243a11d083d7034558
LaupWing <loc_strict@hotmail.com>
2021-12-12 13:48:27 +0100

    left done

--- components/Header.js
+++ components/Header.js
@@ -3,12 +3,24 @@ import Image from 'next/image'
 function Header() {
    return (
       <div>
-         <div className='flex justify-between'>
-            <div className='relative w-24'>
+         <div className='flex justify-between max-w-6xl'>
+            <div className='relative hidden lg:inline-grid h-24 w-24 cursor-pointer'>
                <Image
                   src='https://links.papareact.com/ocw'
                   layout='fill'
+                  objectFit='contain'
                />
+            </div>
+            <div className='relative lg:hidden h-10 w-10 flex-shrink-0 cursor-pointer'>
+               <Image
+                  src='https://links.papareact.com/jjm'
+                  layout='fill'
+                  objectFit='contain'
+               />
+            </div>
+
+            <div>
+               
             </div>
          </div>
       </div>
