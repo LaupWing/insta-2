745a52a4c1e49ff5f65245ba5b11cb0396cf177c
LaupWing <loc_strict@hotmail.com>
2021-12-13 17:54:53 +0100

    added global css

--- components/Header.js
+++ components/Header.js
@@ -39,10 +39,10 @@ function Header() {
             </div>
 
             <div className='flex items-center justify-end space-x-4'>
-               <HomeIcon className='h-10 w-10'/>
+               <HomeIcon className='navBtn'/>
                <MenuIcon className='h-6 md:hidden cursor-pointer'/>
 
-               <PaperAirplaneIcon className='h-10 w-10'/>
+               <PaperAirplaneIcon className='navBtn'/>
             </div>
          </div>
       </div>
