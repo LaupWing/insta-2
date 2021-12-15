5d6ad3bcee91f5daec3d002bf5f61881730fd707
LaupWing <loc_strict@hotmail.com>
2021-12-15 15:04:04 +0100

    update

---
+++ components/Posts.js
@@ -0,0 +1,18 @@
+import Post from "./Post"
+
+const DUMMY_DATA = [
+   {
+      id: '123',
+      username: 'sssssangha'
+   }
+]
+
+const Posts = () => {
+   return (
+      <div>
+         <Post/>     
+      </div>
+   )
+}
+
+export default Posts
