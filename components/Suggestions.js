42fb6a1441171038e161d72b6d9311d97854e116
LaupWing <loc_strict@hotmail.com>
2021-12-17 15:42:14 +0100

    suggestions

--- components/Suggestions.js
+++ components/Suggestions.js
@@ -1,4 +1,15 @@
+import { useEffect, useState } from "react"
+import faker from 'faker'
+
 const Suggestions = () => {
+   const [suggestions, setSuggestions] = useState([])
+
+   useEffect(()=>{
+      const suggestions = [...Array(5)].map((_, i)=>({
+         ...faker.helpers.contextualCard
+      }))
+   }, [])
+
    return (
       <div>
          
