fcab3def8ca0ddabcf15ccdf5d7ca2e43780a095
LaupWing <loc_strict@hotmail.com>
2021-12-17 15:44:58 +0100

    faker file

---
+++ helpers/faker.js
@@ -0,0 +1,13 @@
+export default (total)=>{
+   const suggestions = [...Array(total)].map((_, i)=>({
+      ...faker.helpers.contextualCard(),
+      id: i
+   }))
+   const fetchImages = await fetch('https://randomuser.me/api/?results=20')
+   const images = await fetchImages.json() 
+   
+   return suggestions.map((suggestion, i)=>({
+      ...suggestion,
+      avatar: images.results[i].picture.thumbnail
+   }))
+}
