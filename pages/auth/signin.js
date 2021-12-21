754c21d72e57e0f2280563fd5a58aaf5058436bd
LaupWing <loc_strict@hotmail.com>
2021-12-21 16:43:55 +0100

    signininpage

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,15 +1,29 @@
 import { getProviders, signIn } from 'next-auth/react'
+import Header from '../../components/Header'
 
 const signin = ({ providers }) => {
    return (
       <>
-         {Object.values(providers).map((provider) => (
-            <div key={provider.name}>
-               <button onClick={() => signIn(provider.id)}>
-                  Sign in with {provider.name}
-               </button>
+         <Header/>
+         <div className='flex flex-col items-center justify-center min-h-screen py-2 -mt-56 px-14 text-center'> 
+            <img 
+               src="https://links.papareact.com/ocw" 
+               alt="logo" 
+               className='w-80'
+            />
+            <div className='mt-40'>
+               {Object.values(providers).map((provider) => (
+                  <div key={provider.name}>
+                     <button 
+                        className='p-3 bg-blue-500 rounded-lg text-white'
+                        onClick={() => signIn(provider.id)}
+                     >
+                        Sign in with {provider.name}
+                     </button>
+                  </div>
+               ))}
             </div>
-         ))}
+         </div>
       </>
    )
 }
