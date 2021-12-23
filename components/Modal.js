de4c8427cf978d1a0486c3b0fc20ef96e2501b58
LaupWing <loc_strict@hotmail.com>
2021-12-23 16:01:10 +0100

    update

--- components/Modal.js
+++ components/Modal.js
@@ -5,7 +5,7 @@ import { modalState } from "../atoms/modalAtom"
 
 const Modal = () => {
     const [open, setOpen] = useRecoilState(modalState)
-    
+    console.log(open)
     return (
         <Transition.Root show={open} as={Fragment}>
             <Dialog as='div' className='fixed z-10 inset-0 overflow-y-auto' onClose={setOpen}>
@@ -38,7 +38,36 @@ const Modal = () => {
                         leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                     >
                         <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6">
-                            <h1>Hello</h1>
+                            <div>
+                                <div>
+                                    <div className="mt-3 text-center sm:mt-5">
+                                        <Dialog.Title
+                                            as="h3"
+                                            className="text-lg leading-6 font-medium text-gray-600"
+                                        >
+                                            Upload a photo
+                                        </Dialog.Title>
+                                        <div>
+                                            <input type="file" hidden/>
+                                        </div>
+                                        <div className="mt-2">
+                                            <input 
+                                                type="text" 
+                                                className="border-non focus:ring-0 w-full text-center"
+                                                placeholder="Please enter a caption"
+                                            />
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="mt-5 sm:mt-6">
+                                    <button
+                                        type="button"
+                                        className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:text-sm disabled:bg-gray-300 disabled:cursor-not-allowed hover:disabled:bg-gray-300"
+                                    >
+                                        Upload post
+                                    </button>
+                                </div>
+                            </div>
                         </div>
                     </Transition.Child>
                 </div>
