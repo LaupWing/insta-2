37c216383c92b80ba735ef16d5550f1c66fea973
LaupWing <loc_strict@hotmail.com>
2021-12-24 13:13:58 +0100

    posts

--- components/Posts.js
+++ components/Posts.js
@@ -1,39 +1,49 @@
+import { useEffect, useState } from "react"
 import Post from "./Post"
+import {collection, onSnapshot, orderBy, query} from 'firebase/firestore'
+import { db } from "../firebase"
 
-const posts = [
-   {
-      id: '123',
-      username: 'sssssangha',
-      userImg: 'https://avatars.githubusercontent.com/u/499550?v=4',
-      img: 'https://avatars.githubusercontent.com/u/499550?v=4',
-      caption: 'This is DOPE!'
-   },
-   {
-      id: '12',
-      username: 'sssssangha',
-      userImg: 'https://avatars.githubusercontent.com/u/499550?v=4',
-      img: 'https://avatars.githubusercontent.com/u/499550?v=4',
-      caption: 'This is DOPE!'
-   },
-]
+// const posts = [
+//    {
+//       id: '123',
+//       username: 'sssssangha',
+//       userImg: 'https://avatars.githubusercontent.com/u/499550?v=4',
+//       img: 'https://avatars.githubusercontent.com/u/499550?v=4',
+//       caption: 'This is DOPE!'
+//    },
+//    {
+//       id: '12',
+//       username: 'sssssangha',
+//       userImg: 'https://avatars.githubusercontent.com/u/499550?v=4',
+//       img: 'https://avatars.githubusercontent.com/u/499550?v=4',
+//       caption: 'This is DOPE!'
+//    },
+// ]
 
 const Posts = () => {
-   return (
-      <div>
-         {posts.map(post=>(
-            <Post
-               key={post.id}
-               id={post.id}
-               username={post.username}
-               userImg={post.userImg}
-               img={post.img}
-               caption={post.caption}
-            />     
-         ))
+    const [posts, setPosts] = useState([])
 
-         }
-      </div>
-   )
+    useEffect(()=>{
+        return onSnapshot(query(collection(db, 'posts'), orderBy('timestamp', 'desc')), snapshot =>{
+            setPosts(snapshot.docs)
+        })
+    }, [db])
+    return (
+        <div>
+            {posts.map(post => (
+                <Post
+                    key={post.id}
+                    id={post.id}
+                    username={post.data().username}
+                    userImg={post.data().profileImg}
+                    img={post.data().image}
+                    caption={post.data().caption}
+                />
+            ))
+
+            }
+        </div>
+    )
 }
 
 export default Posts
